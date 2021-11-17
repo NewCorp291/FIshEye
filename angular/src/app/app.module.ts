29eb403ed5126b8afb4506ada40a4c5efc395177
Etienne <newcorp.pro@gmail.com>
2021-11-17 17:15:09 +0100

    feat : navbarComponent + galleryComponent + accueilComponent

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { GalleryComponent } from './gallery/gallery.component';
+
+const appRoutes: Routes = [
+  {path: "navbar", component: NavbarComponent},
+  {path: "accueil", component: AccueilComponent},
+  {path: "gallery", component: GalleryComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AccueilComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
